66822db73e690c9c4d3b31d72deaf6a3286a3e12
Wojtek Padula <wojciech.padula@gmail.com>
2017-04-03 14:25:36 +0100

    * make monitor kubernets compatible

---
+++ start-kafka-monitor.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+KUBEPATH=`pwd`/localhost
+ZK_POD_NAME=`$KUBEPATH/kubectl get pods |grep zoo | awk '{print $1 }'`
+ZK_IP=`$KUBEPATH/kubectl  describe pod $ZK_POD_NAME | grep IP | sed -E 's/IP:[[:space:]]+//' | head -1`
+
+echo $ZK_IP
+
+
+
+docker run --restart=unless-stopped --link -e ZOOKEEPER:$ZK_IP -p 4080:4080 --name kafka-monitor -d wpad/kafka-monitor
+
+
+
+
+
+
+
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 java -cp KafkaOffsetMonitor-assembly-0.2.1.jar \
      com.quantifind.kafka.offsetapp.OffsetGetterWeb \
-     --zk zookeeper \
+     --zk $ZOOKEEPER \
      --port ${KAFKA_MONITOR_PORT} \
      --refresh 10.seconds \
      --retain 2.days 
